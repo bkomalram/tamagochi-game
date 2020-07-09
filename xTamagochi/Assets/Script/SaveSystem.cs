d2bed1d6e15d482817e2c8387ddba792329b8bd0
bkomalram <bhagwan.ram.komalram@gmail.com>
2020-07-09 18:19:24 -0500

    bkomalram - Save as Binary File
    Prueba utilizando el Binary como base del save local del juego, se salva al ejecutar alguna accion.
    
    Issue: Al salir del juego no se guarda. Lo ideal seria guardar el estado de las variables al momento de salir. Sin embargo en OnApplicationQuit y el OnApplicationPause no funcionan como lo esperaba.
    
    onQuit: Se dispara unicamente cuando el juego hace Destroy, pero eso solamente pasa cuando en Unity estas ejecutandolo y dejas de ejecutarlo. En el celular no sucede.
    
    onPause: Sucede en dos (2) acciones en el celular. onResume o onPause, por lo que al momento de iniciar el juego se hace trigger de este evento, sobreescribiendo el save anterior.

---
+++ xTamagochi/Assets/Script/SaveSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class SaveSystem
+{
+    public static void SavedPlayer(Tamagochi player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/Save.ck";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        TamagochiData data = new TamagochiData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static TamagochiData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/Save.ck";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            TamagochiData data = formatter.Deserialize(stream) as TamagochiData;            
+            stream.Close();
+
+            return data;
+
+        } else
+        {
+            Debug.LogError("Save File not found");
+            return null;
+        }
+    }
+}
---
+++ xTamagochi/Assets/Script/moveTamagochi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTamagochi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(Input.mousePosition);
+        // Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // transform.position = new Vector3(position.x, position.y,transform.position.z);
+    }    
+}
